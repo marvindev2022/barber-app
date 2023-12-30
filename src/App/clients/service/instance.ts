bf4c309babde38aca135f958451c6c07ab3a51dd
Marcus  Roza <mavirolero@gmail.com>
2023-12-30 08:07:51 -0300

    ajustes

--- src/App/clients/service/instance.ts
+++ src/App/clients/service/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://barbe0r-c95q.onrender.com",
   timeout: 10000,  
   headers: {
     'Content-Type': 'application/json',
