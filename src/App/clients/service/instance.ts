f0b281d94e763f84838e93b02ecf73390f795c0e
Marcus  Roza <mavirolero@gmail.com>
2023-12-29 23:06:23 -0300

    Update service instance configuration

--- src/App/clients/service/instance.ts
+++ src/App/clients/service/instance.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',  
-  timeout: 5000,  
+  baseURL: "https://barber-c95q.onrender.com",
+  timeout: 10000,  
   headers: {
     'Content-Type': 'application/json',
    },
