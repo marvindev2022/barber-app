fee6a496f8f77cc5b413449abb2a128de769a097
Marcus  Roza <mavirolero@gmail.com>
2024-01-01 18:20:07 -0300

    baseurl deploy

--- src/App/clients/service/instance.ts
+++ src/App/clients/service/instance.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  // baseURL: "https://barbe0r-c95q.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://barbe0r-c95q.onrender.com",
   timeout: 10000,  
   headers: {
     'Content-Type': 'application/json',
